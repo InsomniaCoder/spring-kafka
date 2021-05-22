a1a146f8dc9b833f041da74a9a821fa4e6fb5a79
tanat.l <tanat.l@monix.co.th>
2021-05-22 16:12:07 +0700

    add log payload in controller

--- src/main/java/com/insomniacoder/springkafkanas/controllers/KafkaProducerControllerImpl.java
+++ src/main/java/com/insomniacoder/springkafkanas/controllers/KafkaProducerControllerImpl.java
@@ -3,6 +3,7 @@ package com.insomniacoder.springkafkanas.controllers;
 import com.insomniacoder.springkafkanas.controllers.interfaces.KafkaProducerController;
 import com.insomniacoder.springkafkanas.models.MessagePayload;
 import com.insomniacoder.springkafkanas.services.interfaces.MessageProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RestController
 @RequestMapping("/send-payload")
 public class KafkaProducerControllerImpl implements KafkaProducerController {
+
+
 
     @Autowired
     private MessageProducer producer;
@@ -21,6 +25,7 @@ public class KafkaProducerControllerImpl implements KafkaProducerController {
     @PostMapping
     @Override
     public void sendMessage(@Valid @RequestBody MessagePayload messagePayload) {
+        log.info("Message payload : {}", messagePayload);
         producer.produceMessage(messagePayload);
     }
 }
