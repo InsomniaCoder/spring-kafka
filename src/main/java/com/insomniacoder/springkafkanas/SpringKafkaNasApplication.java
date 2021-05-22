da2a1632ced50528f7df259d26273f0991f1ff60
tanat.l <tanat.l@monix.co.th>
2021-05-22 15:11:53 +0700

    add initial project structure and gitignore

---
+++ src/main/java/com/insomniacoder/springkafkanas/SpringKafkaNasApplication.java
@@ -0,0 +1,13 @@
+package com.insomniacoder.springkafkanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaNasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaNasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insomniacoder/springkafkanas/SpringKafkaNasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insomniacoder.springkafkanas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaNasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
